fab6da8af48fced27316c9efc583ee52c77b6ab6
ijma8870 <jean-baptiste.martin@pole-emploi.fr>
2018-03-16 15:44:27 +0100

    add google home state machine

---
+++ google_home_state_machine.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import threading
+from inspect import signature
+
+from . import app
+
+
+class GoogleHomeStateMachine:
+    KEYWORD_TO_METHOD = {
+        "messenger": app.say_on_facebook_messenger
+    }
+
+    def __init__(self):
+        self.threadLock = threading.Lock()
+        self.state = 'WAITING_METHOD'
+        self.method = None
+        self.arg_number = 0
+        self.params = []
+
+    def process(self, token):
+        self.threadLock.acquire()
+
+        if self.state == 'WAITING_METHOD':
+            self.method = GoogleHomeStateMachine.KEYWORD_TO_METHOD[token]
+            self.arg_number = len(signature(self.method).parameters)
+            self.state = 'WAITING_PARAM'
+
+        elif self.state == 'WAITING_PARAM':
+
+            if token == "cancel":
+                self._reset()
+
+            else:
+                self.params.append(token)
+                if len(self.params) == self.arg_number:
+                    self.method(*self.params)
+                    self._reset()
+
+        self.threadLock.release()
+
+    def _reset(self):
+        self.method = None
+        self.arg_number = 0
+        self.params = []
+        self.state = 'WAITING_METHOD'
