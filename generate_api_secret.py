2cbc65d3a77d6706e8c56c088d3fff28732452b6
jbmartinlengow <jean-baptiste.martin@lengow.com>
2018-04-12 22:17:20 +0200

    bug correction: no chromecast found

--- generate_api_secret.py
+++ generate_api_secret.py
@@ -6,5 +6,5 @@ import string
 KEY_LENGTH = 92
 
 if __name__ == '__main__':
-    api_secret = ''.join(random.choice(string.ascii_letters + string.digits + '+/') for _ in range(KEY_LENGTH))
+    api_secret = ''.join(random.choice(string.ascii_letters + string.digits + '/') for _ in range(KEY_LENGTH))
     print("Your api secret is %s" % api_secret)
