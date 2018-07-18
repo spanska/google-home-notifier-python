c4c6feb328cded08f6d12bef1bb86203da0322aa
ijma8870 <jean-baptiste.martin@pole-emploi.fr>
2018-07-18 10:53:04 +0200

    correction

--- connectors/youtube.py
+++ connectors/youtube.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import asyncio
 import logging
 import re
 from pathlib import Path
@@ -11,8 +10,7 @@ import youtube_dl
 USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/40.0"
 
 
-def find_and_download_song(search_query, follow_playlist=False):
-
+def find_and_download_song(search_query):
     r = requests.post(
         'https://www.youtube.com/results',
         data={'search_query': search_query},
@@ -20,31 +18,14 @@ def find_and_download_song(search_query, follow_playlist=False):
     )
     result = re.search(r'href=\"/watch\?v=(.{11})', r.text).group()[-11:]
     if result:
-
         logging.info("Song find with tag %s" % result)
-        return asyncio.get_event_loop().run_until_complete(_download_song(result))
+        return _download_song(result)
 
     else:
         raise Exception("No Youtube result found for '%s'" % search_query)
 
 
-async def _find_next_song(video_id):
-    r = requests.get(
-        'http://www.youtube.com/watch?v=%s' % video_id,
-        headers={"User-Agent": USER_AGENT}
-    )
-
-    result = re.search(r'href=\"/watch\?v=(.{11})', r.text).group()[-11:]
-    if result:
-
-        logging.info("next song found with tag %s" % result)
-        return result
-
-    else:
-        raise Exception("No next song found for '%s'" % video_id)
-
-
-async def _download_song(video_id):
+def _download_song(video_id):
     filename = None
 
     def _play_hook(d):
@@ -60,6 +41,3 @@ async def _download_song(video_id):
         ydl.download(['http://www.youtube.com/watch?v=%s' % video_id])
 
     return Path(filename)
-
-
-print(find_and_download_song("U2"))
